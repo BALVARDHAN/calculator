0eb36f71faad7fc1913c1345994c788f1d6612de
Balvardhan Parihar <68677559+BALVARDHAN@users.noreply.github.com>
2021-06-01 05:32:18 +0530

    Removed a silly bug causing by zero buttons

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const screen = document.querySelector(".screen");
 const button = document.querySelectorAll(".button");
 const clear = document.querySelector(".button-clear");
 const equal = document.querySelector(".button-equal");
+const dot = document.querySelector(".dot");
 var beginning = true;
 
 button.forEach((elm,idx,arr)=>{
     elm.addEventListener("click",(e)=>{
         if(beginning){
-            screen.innerHTML = elm.textContent;
-            beginning = false;
+            if(elm.textContent == "0" || elm.textContent == "00"){
+                screen.innerHTML = "0"
+            }
+            else{
+                screen.innerHTML = elm.textContent;
+                beginning = false;
+            }
         }        
         else{
             screen.innerHTML += elm.textContent;
@@ -19,6 +25,7 @@ button.forEach((elm,idx,arr)=>{
 clear.addEventListener("click",()=>{
     screen.innerHTML = 0;
     beginning = true;
+
 })
 
 equal.addEventListener("click",()=>{
